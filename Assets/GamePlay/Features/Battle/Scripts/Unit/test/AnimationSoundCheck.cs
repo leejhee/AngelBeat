2d254ce923de911c7d997260a56986f7d7d1d2f3
hance0103 <83891076+hance0103@users.noreply.github.com>
2025-10-30 22:45:54 +0900

    연 애니메이션 스프라이트 설정 및 이미지파일 경로 변경

---
+++ Assets/GamePlay/Features/Battle/Scripts/Unit/test/AnimationSoundCheck.cs
@@ -0,0 +1,49 @@
+using Core.Scripts.Foundation.Define;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationSoundCheck : MonoBehaviour
+{
+    private static readonly int SmokeStepJumpIn = Animator.StringToHash("SmokeStepJumpIn");
+    public AudioSource audioSource;
+    public AudioClip audioClip;
+    public Animator animator;
+    public YeonSkill skill;
+    public void PlaySound()
+    {
+        audioSource.PlayOneShot(audioClip);
+    }
+
+    public void ToIdle()
+    {
+        animator.SetTrigger("Idle");
+    }
+    public void PlayAnimation()
+    {
+        animator.SetTrigger(skill.ToString());
+    }
+
+    public void PlayJumpInTrigger()
+    {
+        animator.SetTrigger(SmokeStepJumpIn);
+    }
+
+    public void PlayAudioClip(AudioClip sound)
+    {
+        audioSource.PlayOneShot(sound);
+    }
+    
+    
+    
+    public enum YeonSkill
+    {
+        MungeCloud,
+        Twister,
+        SmokeWave,
+        SmokeStepCast,
+        SmokeStepJumpIn,
+        SmokeStepJumpOut,
+        SmokeBind
+    }
+}
