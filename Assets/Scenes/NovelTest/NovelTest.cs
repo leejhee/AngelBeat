ddacf5690cc0bd06199814b0249f58c1de881439
Minwoo <168280537+MyoLynn@users.noreply.github.com>
2025-10-30 19:52:26 +0900

    6-3까지 수정

--- Assets/Scenes/NovelTest/NovelTest.cs
+++ Assets/Scenes/NovelTest/NovelTest.cs
@@ -15,7 +15,7 @@ public class NovelTest : MonoBehaviour
     {
         await NovelManager.InitAsync();
         //NovelManager.Instance.PlayTutorial(1);
-        NovelManager.Instance.PlayScript("5");
+        NovelManager.Instance.PlayScript("6_3");
         // var bgm = await SoundManager.Instance.LoadAudioClipByAddressables("BattleBGM");
         //
         // if (SoundManager.Instance!= null)
