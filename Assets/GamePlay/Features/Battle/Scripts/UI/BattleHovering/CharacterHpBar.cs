7142909249fb3d3e9cdb29d482f83b1ff3201dbe
hance0103 <83891076+hance0103@users.noreply.github.com>
2025-11-09 17:11:02 +0900

    체력바 증감 연출

--- Assets/GamePlay/Features/Battle/Scripts/UI/BattleHovering/CharacterHpBar.cs
+++ Assets/GamePlay/Features/Battle/Scripts/UI/BattleHovering/CharacterHpBar.cs
@@ -1,3 +1,4 @@
+using Cysharp.Threading.Tasks;
 using GamePlay.Features.Battle.Scripts.Unit;
 using System;
 using UnityEngine;
@@ -21,37 +22,50 @@ namespace GamePlay.Features.Battle.Scripts.UI.BattleHovering
             if (_mp == null)
                 _mp = new MaterialPropertyBlock();
         }
+        
 
-        public void SetFillAmount(float now, float max)
+        public void SetFillAmount(float target, float max)
         {
-            if (_mp == null)
-                _mp = new MaterialPropertyBlock();
-
-            if (hpBar == null)
-                return;
-            hpBar.GetPropertyBlock(_mp);
-            _mp.SetFloat("_Fill", Mathf.Clamp01(now/max));
-            hpBar.SetPropertyBlock(_mp);
+            SetFillAmount(target / max);
         }
-
-        public void SetFillAmount(float amount)
+        
+        public async void SetFillAmount(float amount)
         {
             if (_mp == null)
                 _mp = new MaterialPropertyBlock();
 
             if (hpBar == null)
                 return;
+            
             hpBar.GetPropertyBlock(_mp);
+            
+            float startValue = _mp.GetFloat("_Fill"); // 현재 fill 값 가져오기
+            float duration = 0.1f;
+            float curDuration = 0f;
+            
+            while (curDuration < duration)
+            {
+                curDuration += Time.deltaTime;
+                float t = curDuration / duration;
+                float newValue = Mathf.Lerp(startValue, amount, t); // 선형 보간
+                fillAmount = newValue;
+                _mp.SetFloat("_Fill", Mathf.Clamp01(newValue));
+                hpBar.SetPropertyBlock(_mp);
+
+                await UniTask.Yield(); // 다음 프레임까지 대기
+            }
+            
             _mp.SetFloat("_Fill", Mathf.Clamp01(amount));
             hpBar.SetPropertyBlock(_mp);
         }
         private void Update()
         {
-            #if UNITY_EDITOR
-            // 에디터에서만 자동 갱신
-            if (!Application.isPlaying)
-                SetFillAmount(fillAmount);
-            #endif
+            //SetFillAmount(fillAmount);
+            // #if UNITY_EDITOR
+            // // 에디터에서만 자동 갱신
+            // if (!Application.isPlaying)
+            //     SetFillAmount(fillAmount);
+            // #endif
         }
     }
 }
