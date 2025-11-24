4e092dcbeccd8b868ef5ae0789eef35fe15700b7
hance0103 <83891076+hance0103@users.noreply.github.com>
2025-11-24 18:13:54 +0900

    탐사 ui 수정

--- Assets/GamePlay/Features/Battle/Scripts/UI/IngameUI/FloatingKeyword.cs
+++ Assets/GamePlay/Features/Battle/Scripts/UI/IngameUI/FloatingKeyword.cs
@@ -1,3 +1,4 @@
+using GamePlay.Features.Battle.Scripts;
 using GamePlay.Features.Battle.Scripts.Models;
 using System.Collections;
 using System.Collections.Generic;
@@ -8,12 +9,12 @@ using UnityEngine.UI;
 
 public class FloatingKeyword : MonoBehaviour
 {
-    [SerializeField] private Image icon;
+    [SerializeField] private SpriteRenderer icon;
     [SerializeField] private TMP_Text count;
     [SerializeField] private TMP_Text amount;
     // 키워드 타입 이넘으로 받아오기 <= 동일 키워드 중복 부여시 amount, count 수정
     [SerializeField] private BattleKeyword keyword;
-    
+
     public void OnInstantiated(FloatingKeywordModel model)
     {
         keyword = model.Keyword;
